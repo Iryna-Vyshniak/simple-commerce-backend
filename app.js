c8991fb8250b99b48e8f7c9a1f3ac60ddf2d2b73
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-04 13:12:55 +0300

    add middleware for routes

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ require('dotenv').config();
 
 const app = express();
 
+const routes = require('./routes/api');
+
 const formatLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatLogger));
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+
+app.use('/api', routes);
 
 module.exports = app;
