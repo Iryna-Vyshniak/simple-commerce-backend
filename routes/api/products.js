755d16a47b79ec2cfbcddc8535dddb49b1058e07
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-18 22:19:45 +0300

    complete with router for get all products

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
+const ctrl = require('../../controllers/products');
 
 const router = Router();
+
+router.get('/', ctrl.getAllProducts);
 
 module.exports = router;
