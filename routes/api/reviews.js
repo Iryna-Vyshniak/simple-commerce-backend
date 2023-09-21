05770bad5d2aaff5e4bdba24aef70fe8b247d2eb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-21 16:43:20 +0300

    added router for get all reviews

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
+const ctrl = require('../../controllers/reviews');
 
 const router = Router();
+
+router.get('/', ctrl.getAllReviews);
 
 module.exports = router;
