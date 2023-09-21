13823d4ab495f80cb55c5046ea7b651cea098b1c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-21 17:43:53 +0300

    added router for get products by id

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const ctrl = require('../../controllers/products');
+const { isValidId } = require('../../middlewares');
 
 const router = Router();
 
 router.get('/', ctrl.getAllProducts);
+router.get('/:id', isValidId, ctrl.getById);
 
 module.exports = router;
