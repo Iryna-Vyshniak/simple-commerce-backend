47cec5a3781361110808f06e17e0312f681f927f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-22 03:23:12 +0300

    complete with $inc

--- controllers/products/getById.js
+++ controllers/products/getById.js
@@ -5,7 +5,11 @@ const { HttpError } = require('../../helpers');
 const getById = async (req, res) => {
   const productId = req.params.id;
 
-  const product = await Product.findById({ _id: productId });
+  const product = await Product.findOneAndUpdate(
+    { _id: productId },
+    { $inc: { viewsCount: 1 } },
+    { new: true }
+  );
 
   if (!product) {
     throw HttpError(404, 'Not Found');
