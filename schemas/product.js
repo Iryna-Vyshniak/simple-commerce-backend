e23bea4e600ae43f4211582823b0f5d2d3c30306
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-22 03:22:38 +0300

    complete model and schema for products

--- schemas/product.js
+++ schemas/product.js
@@ -1,30 +1,26 @@
 const Joi = require('joi');
 
 const validProduct = Joi.object({
-  imgURL: Joi.string()
-    .allow('')
-    .optional(),
-  name: Joi.string()
-    .max(250)
-    .required()
-    .label('Title')
-    .messages({
-      'any.required': 'Title is required.',
-    }),
-  price: Joi.number()
-    .max(5)
-    .min(0)
-    .integer()
-    .required(),
+  imgURL: Joi.string().allow('').optional(),
+  gallery: Joi.object({
+    thumbnail: Joi.string().allow('').optional(),
+    bigShoe: Joi.string().allow('').optional(),
+  }),
+  name: Joi.string().max(250).required().label('Title').messages({
+    'any.required': 'Title is required.',
+  }),
+  rating: Joi.number().max(5).min(0).integer().required(),
+  price: Joi.number().integer().required(),
+  promo: Joi.string().allow('').optional(),
   description: Joi.string(),
   popular: Joi.object({
-    thumbnail: Joi.string()
-      .allow('')
-      .optional(),
-    bigShoe: Joi.string()
-      .allow('')
-      .optional(),
+    thumbnail: Joi.string().allow('').optional(),
+    bigShoe: Joi.string().allow('').optional(),
   }),
+  category: Joi.array().items(Joi.string()).required(),
+  color: Joi.array().items(Joi.string()).required(),
+  size: Joi.array().items(Joi.string()).required(),
+  viewsCount: Joi.number().integer().min(0).required(),
 });
 
 module.exports = validProduct;
